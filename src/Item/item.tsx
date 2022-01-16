06f798858112d7562f49bb85e7627b659e37b0e4
AlwinaO <aoyewole2586@gmail.com>
2022-01-16 00:43:23 -0500

    Add functionality and styles to shopping cart, cart items

--- src/Item/item.tsx
+++ src/Item/item.tsx
@@ -4,7 +4,7 @@ import Button from "@material-ui/core/Button";
 import { CartItemType } from "../App";
 
 // Styles
-import { Wrapper } from "../App.styles";
+import { Wrapper } from './item.styles';
 
 type Props = {
     item: CartItemType;
@@ -17,7 +17,7 @@ const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
         <div>
             <h3>{item.title}</h3>
             <p>{item.description}</p>
-            <h3>{item.price}</h3>
+            <h3>${item.price}</h3>
         </div>
         <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
     </Wrapper>
